3c709d58344079a80660d9d2ef03e2fe1fe2370e
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-11-03 09:49:58 -0500

    Add Event Related Tests

---
+++ tests/test_events.py
@@ -0,0 +1,64 @@
+import importlib
+import os
+import unittest
+
+from datetime import datetime, date, time, timedelta
+
+class TestEvents(unittest.TestCase):
+    def test_create_event(self):
+        DB_NAME = "create_event.test"
+
+        ride = importlib.import_module("open-container.ride")
+
+        conn = ride.load_database(DB_NAME)
+
+        event_time = datetime.now() + timedelta(days=1)
+
+        ride.add_event(conn, event_time, "My Test Event")
+
+        event_time = event_time + timedelta(hours=4)
+
+        ride.add_event(conn, event_time, "My Event", "A Test Event")
+
+        os.remove(DB_NAME)
+        pass
+
+    def test_list_event(self):
+        DB_NAME = "list_event.test"
+
+        ride = importlib.import_module("open-container.ride")
+
+        conn = ride.load_database(DB_NAME)
+
+        event_time = datetime.now() + timedelta(days=1)
+
+        ride.add_event(conn, event_time, "My Test Event")
+
+        event_time = event_time + timedelta(hours=4)
+
+        ride.add_event(conn, event_time, "My Event", "A Test Event")
+
+        ride.list_events(conn)
+
+        os.remove(DB_NAME)
+        pass
+
+    def test_remove_event(self):
+        DB_NAME = "create_event.test"
+
+        ride = importlib.import_module("open-container.ride")
+
+        conn = ride.load_database(DB_NAME)
+
+        event_time = datetime.now() + timedelta(days=1)
+
+        ride.add_event(conn, event_time, "My Test Event")
+
+        event_time = event_time + timedelta(hours=4)
+
+        wild_party = ride.add_event(conn, event_time, "My Event", "A Test Event")
+
+        ride.remove_event(conn, wild_party)
+
+        os.remove(DB_NAME)
+        pass
